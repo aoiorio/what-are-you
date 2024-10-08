66e1cf6a09c9550cef5caf9a39e97cd427f40850
aoiorio <130330538+aoiorio@users.noreply.github.com>
2024-10-08 15:35:03 +0900

    feat: ui of book store app

--- what_are_you_project/what_are_you_app/forms.py
+++ what_are_you_project/what_are_you_app/forms.py
@@ -4,14 +4,13 @@ from .models import Product
 
 class SearchForm(forms.Form):
     query = forms.CharField(
-            label='検索キーワード',
-            max_length=100,  # NOTE assignment 1
-            required=False,
-            widget=forms.TextInput(attrs={'placeholder': 'search me if you can'})
+        max_length=100,  # NOTE assignment 1
+        required=False,
+        widget=forms.TextInput(attrs={"placeholder": "search me if you can"}),
     )
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'description', 'price', 'category']
+        fields = ["name", "description", "price", "category"]
